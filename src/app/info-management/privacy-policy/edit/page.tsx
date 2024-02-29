75aa74ba34ce4670d9bfd6b8108b9a0a0acbf0f1
adhiraj23ideausher <adhiraj.kar@ideausher.com>
2024-02-29 16:18:03 +0530

    removed jodit

--- src/app/info-management/privacy-policy/edit/page.tsx
+++ src/app/info-management/privacy-policy/edit/page.tsx
@@ -1,24 +1,22 @@
 "use client";
 import HeaderBackBar from "@/app/_components/HeaderBackBar";
-import { useRef, useState } from "react";
-import JoditEditor from "jodit-react";
+// import { useRef, useState } from "react";
+// import JoditEditor from "jodit-react";
 
-const Edit = () => {
-  const editor = useRef(null);
-  const [content, setContent] = useState("");
+export default function PrivacyPolicyEditPage() {
+  // const editor = useRef(null);
+  // const [content, setContent] = useState("");
   return (
     <div>
       <HeaderBackBar title="UPDATE PRIVACY POLICY" />
       <div className="bg-white rounded m-10 p-5 shadow-md">
         <p className="text-lg mb-2">Privacy Policy</p>
-        <JoditEditor
+        {/* <JoditEditor
           ref={editor}
           value={content}
           onChange={(newContent) => setContent(newContent)}
-        />
+        /> */}
       </div>
     </div>
   );
-};
-
-export default Edit;
+}
--- src/app/info-management/terms-conditions/edit/page.tsx
+++ src/app/info-management/terms-conditions/edit/page.tsx
@@ -1,23 +1,21 @@
 "use client";
 import HeaderBackBar from "@/app/_components/HeaderBackBar";
-import JoditEditor from "jodit-react";
-import { useRef, useState } from "react";
-const Edit = () => {
-  const editor = useRef(null);
-  const [content, setContent] = useState("");
+// import JoditEditor from "jodit-react";
+// import { useRef, useState } from "react";
+export default function TermsConditionsEditPage() {
+  // const editor = useRef(null);
+  // const [content, setContent] = useState("");
   return (
     <div>
       <HeaderBackBar title="UPDATE TERMS & CONDITIONS" />
       <div className="bg-white rounded m-10 p-5 shadow-md">
         <p className="text-lg mb-2">Privacy Policy</p>
-        <JoditEditor
+        {/* <JoditEditor
           ref={editor}
           value={content}
           onChange={(newContent) => setContent(newContent)}
-        />
+        /> */}
       </div>
     </div>
   );
-};
-
-export default Edit;
+}
